58516223f274c0209595a3109e67e9f704a1865a
Exotic Coder <johnathanbeth001@gmail.com>
2023-05-17 21:22:46 +0800

    update

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -1,8 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  // apiKey: process.env.OPENAI_API_KEY,
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
